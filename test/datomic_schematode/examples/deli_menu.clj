dfa6c44f6cea59cbed625390f9722b5d004595bc
Matt Oquist <moquist@majen.net>
2014-03-15 00:19:12 -0400

    add example usage

---
+++ test/datomic_schematode/examples/deli_menu.clj
@@ -0,0 +1,37 @@
+(ns datomic-schematode.examples.deli-menu
+  (:require [clojure.pprint :refer [pprint]]
+            [datomic.api :as d]
+            [datomic-schematode.core :as ds-core]))
+
+(def db-url "datomic:mem://menudb")
+(d/create-database db-url)
+(def db-conn (d/connect db-url))
+
+(def schema1
+  [:sandwich {:attrs [[:bread-name :string :indexed]
+                      [:meat :string "Many people like meat on their sandwiches"]
+                      [:needs-toothpick :boolean]]}])
+
+(defn step1 []
+  (ds-core/load-schema! db-conn schema1))
+
+(defn step2 []
+  (d/transact db-conn
+              [{:db/id #db/id[:db.part/user]
+                :sandwich/bread-name "focaccia"
+                :sandwich/meat "corned beef"
+                :sandwich/needs-toothpick true}
+               {:db/id #db/id[:db.part/user]
+                :sandwich/bread-name "rye"
+                :sandwich/meat "turky"
+                :sandwich/needs-toothpick false}]))
+
+(defn step3 []
+  (let [db (d/db db-conn)]
+    (pprint (map #(d/touch
+                   (d/entity db
+                             (first %)))
+                 (d/q '[:find ?e
+                        :where [?e :sandwich/bread-name]] db)))))
+
+
