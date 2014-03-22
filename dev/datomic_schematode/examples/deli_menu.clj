ec7e821e5ae6046c2f934ceb2676043c6aa47224
Matt Oquist <moquist@majen.net>
2014-03-22 00:00:24 -0400

    add several examples

--- dev/datomic_schematode/examples/deli_menu.clj
+++ dev/datomic_schematode/examples/deli_menu.clj
@@ -48,3 +48,119 @@
                              :where [?e :sandwich/bread]] db))]
     (pprint entities)
     (count entities)))
+
+
+;; Demonstrate Constraint Features
+;; -------------------
+(def schema2
+  [[:sandwich {:attrs [[:name :string :indexed]
+                       [:bread :enum [:focaccia :wheat :maize :rice] :indexed]
+                       [:meat :string "Many people like meat on their sandwiches"]
+                       [:needs-toothpick :boolean]]
+               :dbfns [;; We can express any db/fns we want here. If a
+                       ;; db/fn has the
+                       ;; :schematode-constraint-fn/active attribute
+                       ;; with the value true, it will be called as a
+                       ;; schematode constraint fn, which must return
+                       ;; either nil (success) or a message explaining
+                       ;; the violated constraint.
+                       {:db/ident :my-fn ; The :ident will be namespaced! ...in this case, to :sandwich/my-fn
+                        :schematode-constraint-fn/active true ; required
+                        :schematode-constraint-fn/name "Avoid at least one gross sandwich name" ; optional
+                        :schematode-constraint-fn/desc "Sandwiches with gross names repel customers." ; optional
+                        :db/fn (d/function '{:lang :clojure
+                                             :params [db]
+                                             :code (if (empty? (d/q '[:find ?e
+                                                                      :where [?e :sandwich/name "soap-scum"]]
+                                                                    db))
+                                                     nil
+                                                     "Ew. You are not allowed to name a sandwich \"soap-scum\".")})}
+                       ;; We can use helper fns to create common constraints.
+                       (ds-constraints/unique :sandwich :bread :meat)]}]
+   [:salad {:attrs [[:name :string :indexed]
+                    [:base :enum [:lettuce :spinach :pasta :unicorns] :indexed]
+                    [:dressing :enum [:ranch :honey-mustard :italian :ceasar :minoan]]]}]])
+
+(defn step4
+  "You must init-schematode-constraints! before you can use
+   schematode's constraint features."
+  []
+  (ds-core/init-schematode-constraints! db-conn))
+
+(defn step5
+  "schema2 contains db/fns with :schematode-constraint-fn attrs."
+  []
+  (ds-core/load-schema! db-conn schema2))
+
+(defn step6
+  "Can we violate our constraints?"
+  []
+  (d/transact db-conn
+              [[:schematode-tx :enforce [{:db/id (d/tempid :db.part/user)
+                                          :sandwich/name "soap-scum"}
+                                         {:db/id (d/tempid :db.part/user)
+                                          :sandwich/name "Just Rice"
+                                          :sandwich/bread :sandwich.bread/rice
+                                          :sandwich/meat ""}
+                                         {:db/id (d/tempid :db.part/user)
+                                          :sandwich/name "Only Rice"
+                                          :sandwich/bread :sandwich.bread/rice
+                                          :sandwich/meat ""}]]]))
+
+(defn step6
+  "Can we violate our constraints?"
+  []
+  (d/transact db-conn
+              [[:schematode-tx :enforce [{:db/id (d/tempid :db.part/user)
+                                          :sandwich/name "soap-scum"}
+                                         {:db/id (d/tempid :db.part/user)
+                                          :sandwich/name "Just Rice"
+                                          :sandwich/bread :sandwich.bread/rice
+                                          :sandwich/meat ""}
+                                         {:db/id (d/tempid :db.part/user)
+                                          :sandwich/name "Only Rice"
+                                          :sandwich/bread :sandwich.bread/rice
+                                          :sandwich/meat ""}]]]))
+(defn step7
+  "Test our constraints without attempting to transact anything."
+  []
+  (let [my-schematode-tx* (:db/fn (d/entity (d/db db-conn) :schematode-tx*))]
+    (my-schematode-tx* (d/db db-conn)
+                       [{:db/id (d/tempid :db.part/user)
+                         :sandwich/name "soap-scum"}
+                        {:db/id (d/tempid :db.part/user)
+                         :sandwich/name "Just Rice"
+                         :sandwich/bread :sandwich.bread/rice
+                         :sandwich/meat ""}
+                        {:db/id (d/tempid :db.part/user)
+                         :sandwich/name "Only Rice"
+                         :sandwich/bread :sandwich.bread/rice
+                         :sandwich/meat ""}])))
+
+(defn step8
+  "Apply constraint warning messages to the TX entity, but let the
+   transaction go through.
+
+   N.B.: This is not good practice in production, because any
+   violations that are not cleaned up will continue to attach
+   violation messages to every TX entity."
+  []
+  (d/transact db-conn
+              [[:schematode-tx :warn [{:db/id (d/tempid :db.part/user)
+                                       :sandwich/name "soap-scum"}
+                                      {:db/id (d/tempid :db.part/user)
+                                       :sandwich/name "Just Rice"
+                                       :sandwich/bread :sandwich.bread/rice
+                                       :sandwich/meat ""}
+                                      {:db/id (d/tempid :db.part/user)
+                                       :sandwich/name "Only Rice"
+                                       :sandwich/bread :sandwich.bread/rice
+                                       :sandwich/meat ""}]]]))
+
+(defn step9
+  "What is the performance cost of using :schematode-tx?"
+  []
+  (let [db (d/db db-conn)
+        query '[:find ?e :where [?e :schematode-constraint/elapsed-msec]]]
+    (map #(:schematode-constraint/elapsed-msec (d/entity db (first %)))
+         (d/q query db))))
