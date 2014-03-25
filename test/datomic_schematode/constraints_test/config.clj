ff3bbcb08f17633b59c185815cd5c1fde130b9e6
Matt Oquist <moquist@majen.net>
2014-03-25 13:02:55 -0400

    add fixture for constraint tests

---
+++ test/datomic_schematode/constraints_test/config.clj
@@ -0,0 +1,20 @@
+(ns datomic-schematode.constraints-test.config
+  (:require [clojure.test :refer :all]
+            [datomic.api :as d]
+            [datomic-schematode.core :as ds-core]
+            [datomic-schematode.constraints :as ds-constraints]))
+
+(def db-url "datomic:mem://constraints-test")
+
+(def test-schemas
+  [[:a {:attrs [[:a1 :string]
+                [:a2 :string]]
+        :dbfns [(ds-constraints/unique :a :a1 :a2)]}]])
+
+(defn with-schema [f]
+  (d/create-database db-url)
+  (ds-core/init-schematode-constraints! (d/connect db-url))
+  (ds-core/load-schema! (d/connect db-url) test-schemas)
+  (f)
+  (d/delete-database db-url))
+
