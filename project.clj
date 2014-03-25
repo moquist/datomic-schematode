700ca7ffdd147cd1114869928e4d6cc51810069c
Matt Oquist <moquist@majen.net>
2014-03-25 13:36:02 -0400

    update to RC1

--- project.clj
+++ project.clj
@@ -1,13 +1,12 @@
-(defproject datomic-schematode "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject datomic-schematode "0.1.0-RC1"
+  :description "lets you express concise Datomic schema with DB constraints"
+  :url "https://github.com/vlacs/datomic-schematode"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [datomic-schema "1.0.2"]
                  [incanter/incanter-core "1.5.4"]]
-  :profiles {:dev
-             {:source-paths ["dev"]
-              :dependencies [[org.clojure/tools.namespace "0.2.4"]
-                             [com.datomic/datomic-free "0.9.4572"]]}}
+  :profiles {:dev {:source-paths ["dev"]
+                   :dependencies [[org.clojure/tools.namespace "0.2.4"]
+                                  [com.datomic/datomic-free "0.9.4572"]]}}
   :plugins [[lein-cloverage "1.0.2"]])
