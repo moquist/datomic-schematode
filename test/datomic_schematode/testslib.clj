2ea91beff49ada0629a17cfef2261430fd9f7091
Matt Oquist <moquist@majen.net>
2014-03-25 13:03:19 -0400

    add some fns to make tests shorter

---
+++ test/datomic_schematode/testslib.clj
@@ -0,0 +1,21 @@
+(ns datomic-schematode.testslib)
+
+(defmacro should-throw
+  "Borrowed from https://github.com/Datomic/day-of-datomic .
+   Runs forms, expecting an exception. Returns exception message if an
+   exception occurred, and false if no exception occurred."
+  [& forms]
+  `(try
+     ~@forms
+     false
+     (catch Exception t#
+       (str "Got expected exception:\n\t" (.getMessage t#)))))
+
+(defn ensure-tx [tx]
+  (= '(:db-before :db-after :tx-data :tempids) (keys @tx)))
+
+(defn ensure-seq-txs [txs]
+  (every? ensure-tx txs))
+
+(defn predict-entity-map [e]
+  (merge (into {} e) {:db/id -1}))
