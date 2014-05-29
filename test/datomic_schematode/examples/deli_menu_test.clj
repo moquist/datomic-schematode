b78afe5882e4413fa461e558b3903d2c2b125314
Matt Oquist <moquist@majen.net>
2014-05-29 13:00:49 -0400

    fix tests

--- test/datomic_schematode/examples/deli_menu_test.clj
+++ test/datomic_schematode/examples/deli_menu_test.clj
@@ -19,39 +19,39 @@
        (str "Got expected exception:\n\t" (.getMessage t#)))))
 
 (deftest deli-menu-tests-no-constraints
-  (testing "step1!"
-    (is (ds-tl/ensure-seq-txs (deli-menu/step1!))))
-  (testing "step2!"
-    (is (ds-tl/ensure-tx (deli-menu/step2!))))
+  (testing "step2"
+    (is (ds-tl/ensure-seq-txs (deli-menu/step2))))
   (testing "step3"
-    (is (= (let [{:keys [entities count]} (deli-menu/step3)
+    (is (ds-tl/ensure-tx (deli-menu/step3))))
+  (testing "step4"
+    (is (= (let [{:keys [entities count]} (deli-menu/step4)
                  entities (map ds-tl/predict-entity-map entities)
                  entities (into #{} entities)]
              [entities count])
            [#{{:sandwich/needs-toothpick true, :sandwich/meat "corned beef", :sandwich/bread :sandwich.bread/focaccia, :sandwich/name "Norville's #1", :db/id -1} {:sandwich/needs-toothpick false, :sandwich/meat "turkey", :sandwich/bread :sandwich.bread/maize, :sandwich/name "Thanksgiving Leftovers", :db/id -1}} 2]))))
 
 (deftest deli-menu-tests-constraints
-  (testing "step4!"
-    (is (ds-tl/ensure-seq-txs (deli-menu/step4!))))
-  (testing "step5!"
-    (is (ds-tl/ensure-seq-txs (deli-menu/step5!))))
-  (testing "step6!"
-    (is (= (ds-tl/should-throw @(deli-menu/step6!))
-           "Got expected exception:\n\tjava.lang.Exception: [\"Ew. You are not allowed to name a sandwich \\\"soap-scum\\\".\"][\"unique constraint failed for [:sandwich/bread :sandwich/meat]\"]")))
+  (testing "step6"
+    (is (ds-tl/ensure-seq-txs (deli-menu/step6))))
   (testing "step7"
-    (is (= (sort (deli-menu/step7))
+    (is (ds-tl/ensure-seq-txs (deli-menu/step7))))
+  (testing "step8"
+    (is (= (ds-tl/should-throw @(deli-menu/step8))
+           "Got expected exception:\n\tjava.lang.Exception: [\"Ew. You are not allowed to name a sandwich \\\"soap-scum\\\".\"][\"unique constraint failed for [:sandwich/bread :sandwich/meat]\"]")))
+  (testing "step9"
+    (is (= (sort (deli-menu/step9))
            '("Ew. You are not allowed to name a sandwich \"soap-scum\"."
              "unique constraint failed for [:sandwich/bread :sandwich/meat]"))))
   
-  (testing "step8! and step9"
+  (testing "step10 and step11"
     (is (= (sort (keys (first (do
-                                (deli-menu/step8!)
-                                (deli-menu/step9)))))
+                                (deli-menu/step10)
+                                (deli-menu/step11)))))
            '(:db/txInstant :schematode.constraint/elapsed-msec :schematode.constraint/messages))))
 
-  (testing "step10"
-    (is (every? number? (deli-menu/step10))))
+  (testing "step12"
+    (is (every? number? (deli-menu/step12))))
 
-  (testing "step11"
-    (is (= (sort (keys (deli-menu/step11)))
+  (testing "step13"
+    (is (= (sort (keys (deli-menu/step13)))
            '(:mean-msec :median-msec :total-msec :tx-count)))))
